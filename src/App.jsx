388cc97a712b47270172c493b0b1e5b24fe5c4c2
Brian A. Danielak <briandaniela.k+github@gmail.com>
2019-05-08 15:41:58 -0400

    Rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import {} from "./components/counter-initial";
+
+class App extends Component {
+  render() {
+    return <h1>Hello, World!</h1>;
+  }
+}
+
+export default App;
